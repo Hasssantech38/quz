58e8750def28d02ffd82016a7a967e37cfc98bc5
Hasssantech38 <141651660+Hasssantech38@users.noreply.github.com>
2024-07-20 13:17:31 +0500

    new

---
+++ quizz/lib/main.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'Assessment.dart';
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home Page'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => Assessment(),
+              ),
+            );
+          },
+          child: Text('Go to Quiz Page'),
+        ),
+      ),
+    );
+  }
+}
